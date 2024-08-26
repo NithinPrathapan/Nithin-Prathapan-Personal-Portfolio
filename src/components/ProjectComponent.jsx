a46777dfc76cc29ddd49b27dc4b9c6f4944f2be2
Nithin Prathapan <nithinprathapan369@gmail.com>
2024-08-27 03:43:36 +0530

    project componet structure

--- src/components/ProjectComponent.jsx
+++ src/components/ProjectComponent.jsx
@@ -1,28 +1,47 @@
-import React from "react";
+import React, { useRef } from "react";
 import LineGradient from "./LineGradient";
+import { motion, useInView } from "framer-motion";
+import web from "../assets/web.png";
+import git from "../assets/git.png";
 
-const ProjectComponent = () => {
+const ProjectComponent = ({
+  title,
+  url,
+  img,
+  description,
+  animation,
+  delay,
+}) => {
+  const ref = useRef(null);
+  const isInView = useInView(ref, { once: true, amount: 0.2 });
   return (
-    <div className=" w-full flex flex-col justify-between md:flex md:flex-row-reverse md:p-8 p-4 items-center gap-2 rounded-md border border-blue shadow-2xl  shadow-black">
+    <motion.div
+      ref={ref}
+      initial="hidden"
+      animate={isInView ? "visible" : "hidden"}
+      variants={animation}
+      transition={{
+        delay: isInView ? delay : 0,
+        duration: 0.5, // Duration of the animation
+      }}
+      className=" w-full flex flex-col justify-between md:flex md:flex-row-reverse md:p-8 p-4 items-center gap-2 rounded-md  md:border border-blue shadow-xl   shadow-black"
+    >
       <div className="md:w-[45%] relative">
         <img
-          className="rounded-md min-w-full object-cover h-full"
-          src="https://t4.ftcdn.net/jpg/05/50/33/47/360_F_550334715_0d2cdaljV4Xd3x7yVUhRxfmLLEUyMdXr.jpg"
+          className="rounded-md min-w-full object-cover max-h-[380px] h-full"
+          src={img}
           alt=""
         />
       </div>
       <div className="md:w-[55%] md:text-start text-center flex flex-col gap-2">
-        <h1 className="text-2xl md:text-3xl font-playfair font-semibold text-yellow">
-          Project 1
+        <h1 className="text-2xl  font-playfair font-semibold text-yellow">
+          {title}
         </h1>
-        <div className="w-[10%] flex justify-center mx-auto md:mx-0">
+        <div className="w-[15%] flex justify-center mx-auto md:mx-0">
           <LineGradient />
         </div>
         <p className="italic my-2 md:text-start mx-auto md:mx-0 w-[80%] ">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus
-          sunt debitis quos, hic, voluptatem eos ad deserunt eligendi natus
-          consequatur maiores soluta reiciendis. Adipisci facilis asperiores
-          eligendi saepe aliquid earum ullam temporibus.
+          {description}
         </p>
         <div className="flex justify-center md:justify-start md:text-start items-center gap-4 flex-wrap  w-[80%] md:mx-0 mx-auto  ">
           <p className="text-center  font-semibold text-yellow font-playfair">
@@ -35,8 +54,22 @@ const ProjectComponent = () => {
             Lorem
           </p>
         </div>
+        <div className="flex md:justify-start justify-center items-center gap-4 my-12">
+          <img
+            className="w-[35px] hover:scale-[105%]  duration-300 transition-all cursor-pointer"
+            title="Visit website"
+            src={web}
+            alt=""
+          />
+          <img
+            className="w-[35px] hover:scale-[105%] duration-300 transition-all cursor-pointer"
+            title="View code on github"
+            src={git}
+            alt=""
+          />
+        </div>
       </div>
-    </div>
+    </motion.div>
   );
 };
 
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -4,19 +4,6 @@ import { motion } from "framer-motion";
 import ProjectComponent from "./ProjectComponent";
 import LineGradient from "./LineGradient";
 const Projects = () => {
-  const container = {
-    hidden: {},
-    visible: {
-      transition: {
-        staggerChildren: 0.2,
-      },
-    },
-  };
-
-  const projectVariant = {
-    hidden: { opacity: 0, scale: 0.5 },
-    visible: { opacity: 1, scale: 1 },
-  };
   return (
     <section id="projects" className="flex justify-center pt-48 pb-48">
       <div>
@@ -49,12 +36,11 @@ const Projects = () => {
           </p>
         </motion.div>
         <motion.div
-          className="w-full"
-          initial="hidden"
-          whileInView="visible"
+          className="w-full flex flex-col gap-8"
+          initial={{ opacity: 0 }}
+          whileInView={{ opacity: 1 }}
           viewport={{ once: true }}
           transition={{ delay: 0.4, duration: 1 }}
-          variants={container}
         >
           {/* row1  */}
           {/* add github and project link  */}
@@ -64,6 +50,34 @@ const Projects = () => {
         >
           beautiful user ineterface
         </div> */}
+
+          {/* first row */}
+          <ProjectComponent
+            title="Real Estate Application"
+            description={
+              "It has been developed using MERN Stack and designed using Tailwind CSS. It includes modern and responsive design."
+            }
+            skills={[
+              "React.js",
+              "Node.js",
+              "Tailwind CSS",
+              "Express.js",
+
+              "MongoDB",
+              "JWT",
+              "Redux Toolkit",
+              "Tailwind",
+              "Firebase",
+            ]}
+            animation={{
+              hidden: { opacity: 0, x: -100 }, // Slide in from the left
+              visible: { opacity: 1, x: 0 },
+            }}
+            delay={0.5}
+            img={project1}
+          />
+
+          {/* row 2 */}
           <ProjectComponent
             title="Real Estate Application"
             description={
@@ -82,10 +96,63 @@ const Projects = () => {
               "Firebase",
             ]}
             img={project1}
+            animation={{
+              hidden: { opacity: 0, x: 100 }, // Slide in from the left
+              visible: { opacity: 1, x: 0 },
+            }}
+            delay={0.8}
           />
 
-          {/* row 2 */}
+          <div className="md:flex   justify-center items-center gap-4">
+            <ProjectComponent
+              title="Real Estate Application"
+              description={
+                "It has been developed using MERN Stack and designed using Tailwind CSS. It includes modern and responsive design."
+              }
+              skills={[
+                "React.js",
+                "Node.js",
+                "Tailwind CSS",
+                "Express.js",
 
+                "MongoDB",
+                "JWT",
+                "Redux Toolkit",
+                "Tailwind",
+                "Firebase",
+              ]}
+              img={project1}
+              animation={{
+                hidden: { opacity: 0, y: 50 }, // Slide in from the left
+                visible: { opacity: 1, y: 0 },
+              }}
+              delay={0.5}
+            />{" "}
+            <ProjectComponent
+              title="Real Estate Application"
+              description={
+                "It has been developed using MERN Stack and designed using Tailwind CSS. It includes modern and responsive design."
+              }
+              skills={[
+                "React.js",
+                "Node.js",
+                "Tailwind CSS",
+                "Express.js",
+
+                "MongoDB",
+                "JWT",
+                "Redux Toolkit",
+                "Tailwind",
+                "Firebase",
+              ]}
+              img={project1}
+              animation={{
+                hidden: { opacity: 0, y: 50 }, // Slide in from the left
+                visible: { opacity: 1, y: 0 },
+              }}
+              delay={0.3}
+            />
+          </div>
           {/* <div
           className="flex justify-center text-center items-center p-10 bg-blue uppercase 
     max-w-[400px]  text-2xl font-playfair font-semibold"
